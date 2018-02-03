0d4c5be94c81286710900ecd4b82501922c8aeb2
Tong <zt9465@gmail.com>
2018-02-03 17:16:48 -0500

    updated readme

--- input/1.php
+++ input/1.php
@@ -1,41 +1,9 @@
 <?php
 
-///* expr:
-//   ArrayDimFetch
-//   BinaryOp
-//   Assign
-//   FuncCall
-//   Variable
-//   LNumber
-//   String_
-//*/
-//
-//$offset = $argv[0]; // beware, no input validation!
-//$query  = "SELECT id, name FROM products ORDER BY name LIMIT 20 OFFSET $offset;";
-//$query1 = "SELECT ...";
-////$query = $query1;
-//// return type is not string
-//$foolist = sqlit($query, " ");
-//// no return type
-//string_routine($query);
-//$query2 = add_num_to_str($query);
-//$result = pg_query($conn, $query2);
-//
-//
-//
-//$b = $argv[0];
-//$a = (1+2-1)."$b bar";
-//$c = substr(strtolower($a)."lower", 1);
-//pg_query($c);
-//pg_query(f2(($a+1*2)."some"));
-
-
 if(input_from_params()) {
     $a = $_GET[0];
 }
-else {
-    $a = mysql_fetch_row();
-}
+
 system($a);
 
 ?>
